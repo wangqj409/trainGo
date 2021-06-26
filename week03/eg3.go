0ec7c63f1ddbd45080851c1d899ad8c5894caa24
wangqj409 <wangqj409@gmail.com>
2021-06-27 02:58:06 +0800

    rewrite errgroup

---
+++ week03/eg3.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func fn(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/quit" {
+		// special path to exit
+		os.Exit(1)
+	}
+	fmt.Printf("%s\n", r.URL.Path)
+	w.Write([]byte("OK"))
+}
+
+func httpstart() error {
+	serv := http.NewServeMux()
+	serv.HandleFunc("/", fn)
+
+	return http.ListenAndServe(":4000", serv)
+}
+
+func reg() error {
+	sigs := []os.Signal{syscall.SIGINT, syscall.SIGCHLD, syscall.SIGABRT}
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, sigs...)
+	for sig := range sigchan {
+		fmt.Printf("recv signal: %s\n", sig)
+	}
+	return nil
+}
+
+func main() {
+	eg, ctx := errgroup.WithContext(context.Background())
+	eg.Go(func() error {
+		return httpstart()
+	})
+	eg.Go(func() error {
+		<-ctx.Done()
+		fmt.Printf("http server will shutdown\n")
+		return errors.New("shutdown")
+	})
+	eg.Go(reg)
+	if err := eg.Wait(); err != nil {
+		panic(err)
+	}
+}
